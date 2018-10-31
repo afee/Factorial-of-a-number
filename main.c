a88041ef9ce0ac5e1ebc9f1ca833c9284b0a223d
afee <37291431+afee@users.noreply.github.com>
2018-10-31 15:55:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
+//this is a program to find factorial of a number 
 #include <stdio.h>
 #include <stdlib.h>
-
+//this is a function to findfactorial of a number 
 int fact(int num)
 {
     int res;
     if(num==1 || num==0)
-        return 1;
+        return 1;//factorial of number 1 and 0 is 1
     else
-        res=num *fact(num-1);
+        res=num *fact(num-1);// call function fact recursively 
     return res;
 }
 
 int main()
 {
    int i,n;
-   printf("enter the number: ");
+   printf("enter the number: ");//reading the number 
    scanf("%d",&n);
 
-   i=fact(n);
-   printf("%d",i);
+   i=fact(n);//calling the function and return result to variable i
+   printf("%d",i);//printing the result 
 
 
     return 0;
